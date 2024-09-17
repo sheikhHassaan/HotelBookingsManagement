213d1117c3e5c062c7b1f942619f5683588c1468
sheikhHassaan <hassaanbasil@gmail.com>
2024-09-17 08:38:05 +0500

    Initial commit - Project upload.

---
+++ src/main/java/com/project/hotelBookingsManagement/HotelBookingsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.hotelBookingsManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/hotelBookingsManagement/TestHotelBookingsManagementApplication.java
@@ -0,0 +1,11 @@
+package com.project.hotelBookingsManagement;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestHotelBookingsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(HotelBookingsManagementApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
