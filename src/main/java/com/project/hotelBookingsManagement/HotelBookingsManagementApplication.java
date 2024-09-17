6801ea4bd19b67d94129525ccfe1a8d82879e4f1
sheikhHassaan <hassaanbasil@gmail.com>
2024-09-17 08:40:55 +0500

    master - set default server port to 8081.

--- src/main/java/com/project/hotelBookingsManagement/HotelBookingsManagementApplication.java
+++ src/main/java/com/project/hotelBookingsManagement/HotelBookingsManagementApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HotelBookingsManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HotelBookingsManagementApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HotelBookingsManagementApplication.class, args);
+        System.out.println("Hello world!");
+    }
 
 }
