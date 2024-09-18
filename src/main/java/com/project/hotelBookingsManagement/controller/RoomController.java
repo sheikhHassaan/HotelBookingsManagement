6dae7de1cc952fcc8bea459f3e3562dcec37a350
sheikhHassaan <hassaanbasil@gmail.com>
2024-09-18 11:30:29 +0500

    Controller - Implemented Controllers for Payment, Customer, Room, Hotel entities.

--- src/main/java/com/project/hotelBookingsManagement/controller/RoomController.java
+++ src/main/java/com/project/hotelBookingsManagement/controller/RoomController.java
@@ -1,5 +1,43 @@
 package com.project.hotelBookingsManagement.controller;
 
+import com.project.hotelBookingsManagement.domain.Room;
+import com.project.hotelBookingsManagement.service.RoomsServiceImpl;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/hotel_bookings_management/room")
 public class RoomController {
 
+    RoomsServiceImpl roomService;
+
+    @GetMapping("/list")
+    public List<Room> getAllRooms() {
+        return roomService.getAllRooms();
+    }
+
+    @GetMapping("/get/{room_id}")
+    public Room getRoom(@PathVariable("room_id") String roomId) {
+        return roomService.getRoomById(roomId);
+    }
+
+    @PostMapping("/add")
+    public void addRoom(@Validated @RequestBody Room room) {
+        roomService.addRoom(room);
+    }
+
+    @PutMapping("/update/{room_id}")
+    public void updateRoom(@PathVariable("room_id") String roomId, @RequestBody Room room) {
+        if (roomId != null) {
+            room.setRoomId(roomId);
+        }
+        roomService.updateRoom(room);
+    }
+
+    @DeleteMapping("/remove/{room_id}")
+    public void removeRoom(@PathVariable("room_id") String roomId) {
+        roomService.deleteRoom(roomId);
+    }
 }
--- src/main/java/com/project/hotelBookingsManagement/service/CustomersServiceImpl.java
+++ src/main/java/com/project/hotelBookingsManagement/service/CustomersServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class CustomersService {
+public class CustomersServiceImpl {
 
     @Autowired
     CustomerRepository customerRepository;
